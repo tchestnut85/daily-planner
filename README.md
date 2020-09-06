20e5a10257e230409e74d658d64118c1b174cbc5
Tom Chestnut <tchestnut85@gmail.com>
2020-09-06 12:23:10 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Work Day Scheduler Starter Code
 
 ## About this App 
-Paragraph goes here
+This is a workday planner that displays the current date and time that will help the user with scheduling their tasks throughout the day.
+The user is able to enter text into the rows for each hour and clicking on the save icon will save the text in that row.
+The color of the row will change depending if the current time is in the past hour (gray), current hour (red), or future hour (green).
+When the page is refreshed, the tasks for each row will persist.
 
 ### Languages Used
 * Javascript
@@ -11,23 +14,15 @@ Paragraph goes here
 ### APIs Used
 * Bootstrap
 * jQuery
-* jQuery UI
 * Moment.js
-* Open Iconic
+* Font Awesome
 
-### Accessing the Webpage
-https://tchestnut85.github.io/daily-planner/
+### Accessing the Application
+Deployed Application: https://tchestnut85.github.io/daily-planner/
+GitHub Repository Link: https://github.com/tchestnut85/daily-planner/
 
 ### Screenshots
 
 #### Title
 
 <img src=./assets/images/screenshot-1.JPG/>
-
-#### Title
-
-<img src=./assets/images/screenshot-2.JPG/>
-
-#### Title
-
-<img src=./assets/images/screenshot-3.JPG/>
--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,15 @@
 // timeColor task color change function variables
-var currentDate = moment().format("[Today is: ] dddd, MMMM Do YYYY");
-var currentClock = moment().format("[The current time is: ] LT");
 var currentHour = moment().hour();
-var taskBlock = $(".textarea");
 var selectedHour = $(".selected-hour");
 
-// Save function variables
-var taskContent = $("#textarea").data();
-var inputValue = $("#textarea").value;
-var hourArr = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-
+// Clock and time display
+var currentDate = moment().format("[Today is: ] dddd, MMMM Do YYYY");
 $("#currentDay").text(currentDate);
+var currentClock = moment().format("[The current time is: ] LT");
 $("#currentTime").text(currentClock);
+
+// Hour array
+var hourArr = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 // Function to check change color of #textarea depending on time
 var timeColor = function() {
@@ -32,16 +30,20 @@ var timeColor = function() {
     }
 };
 
+
 // Fuction to save text typed into textarea when the save button icon is clicked
 $(".saveBtn").on("click", function(event) {
+    // var target pulls ID of the previous element which is <textarea>
     var target = event.currentTarget.previousElementSibling.id;
     var userInput = $("#" + target).val().trim();
     localStorage.setItem(target, JSON.stringify(userInput));
 });
 
-// Get text-input from localstorage and display into text areas
+// Load Saved text function - Get text-input from localstorage and display into text areas
 function onLoad() {
-    for (var hour of hourArr) {
+    for (var i = 0; i < hourArr.length; i++) {
+    // for (var hour of hourArr) {
+        var hour = hourArr[i];
         var value = JSON.parse(localStorage.getItem("text-area-" + hour));
         $("#text-area-" + hour).val(value);
     }
