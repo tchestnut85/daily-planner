7f8c781f116c11cc384f78f1946e9eaa39117698
Tom Chestnut <tchestnut85@gmail.com>
2020-09-06 00:22:07 -0400

    added functions to change color of rows based on past, current or future times and function for saving and displaying the tasks

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -2,45 +2,50 @@
 var currentDate = moment().format("[Today is: ] dddd, MMMM Do YYYY");
 var currentClock = moment().format("[The current time is: ] LT");
 var currentHour = moment().hour();
-console.log(currentHour);
-var taskBlock = $("#textarea");
+var taskBlock = $(".textarea");
 var selectedHour = $(".selected-hour");
 
 // Save function variables
 var taskContent = $("#textarea").data();
-var savedTask = localStorage.setItem("task", JSON.stringify(taskContent));
+var inputValue = $("#textarea").value;
+var hourArr = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 $("#currentDay").text(currentDate);
 $("#currentTime").text(currentClock);
 
-
-// link display row time to a moment 
-
 // Function to check change color of #textarea depending on time
 var timeColor = function() {
-
     for (var i = 0; i < selectedHour.length; i++) {
+
         var task = $(selectedHour[i]);
-        var timeBlock = $("#time-block");
-        // task.data("hour") ????
-
-    if (timeBlock < currentHour) {
-        task.addClass("past");
-    } 
-    else if (timeBlock === currentHour) {
-        task.addClass("present")
+        var taskHour = task.data().time;
+        
+        if (taskHour < currentHour) {
+            task.addClass("past");
+        } 
+        else if (taskHour > currentHour) {
+            task.addClass("future");
+        }
+        else {
+            task.addClass("present")
+        }
     }
-    else {
-        task.addClass("future")
-    }
-
-    }
-
 };
 
+// Fuction to save text typed into textarea when the save button icon is clicked
+$(".saveBtn").on("click", function(event) {
+    var target = event.currentTarget.previousElementSibling.id;
+    var userInput = $("#" + target).val().trim();
+    localStorage.setItem(target, JSON.stringify(userInput));
+});
+
+// Get text-input from localstorage and display into text areas
+function onLoad() {
+    for (var hour of hourArr) {
+        var value = JSON.parse(localStorage.getItem("text-area-" + hour));
+        $("#text-area-" + hour).val(value);
+    }
+};
+
+onLoad();
 timeColor();
-
-// fuction to save textarea when save button is clicked
-// $("#saveBtn").on("click") function() {
-
-// }
